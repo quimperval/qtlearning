320655dece9ea216573420161670b940e538b1cb
Joaquin <joaquinperezvalera@gmail.com>
2023-11-29 20:54:37 -0600

    First commit

---
+++ todolist/mainwindow.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    /*connect(ui->addTaskButton, &QPushButton::clicked,
+            QApplication::instance, &QApplication::quit);
+            */
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
